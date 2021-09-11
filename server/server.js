261515ff9edae5c6d925837af3fa107f0ed8db07
Nirmal Limbu <nirmalmaswa@live.com>
2021-09-11 22:51:08 +0545

    server : connection to mongodb success

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 const app = require('./app');
 
+const connectDatabase = require('./config/database')
+
 const dotenv = require('dotenv');
 
 // setting up config file
 dotenv.config({ path: 'server/config/config.env' })
+
+// connecting to database
+connectDatabase();
 
 app.listen(process.env.PORT, () => {
     console.log(`server started at PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode. `)
